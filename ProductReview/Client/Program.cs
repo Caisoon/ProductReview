41f60580b7bf29959e49b8beda1ae76ec16e8177
TAN CAI SOON 2101662A <2101662A@student.tp.edu.sg>
2023-02-08 08:30:35 +0800

    Global Error Handling

--- ProductReview/Client/Program.cs
+++ ProductReview/Client/Program.cs
@@ -3,11 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ProductReview.Client.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 namespace ProductReview.Client
 {
@@ -18,11 +20,15 @@ namespace ProductReview.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddHttpClient("ProductReview.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+            builder.Services.AddHttpClient("ProductReview.ServerAPI", (sp,client) => { client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress); client.EnableIntercept(sp); })
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
             // Supply HttpClient instances that include access tokens when making requests to the server project
             builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("ProductReview.ServerAPI"));
+
+            builder.Services.AddHttpClientInterceptor();
+
+            builder.Services.AddScoped<HttpInterceptorService>();
 
             builder.Services.AddApiAuthorization();
 
